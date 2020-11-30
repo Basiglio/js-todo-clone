92ab406f846d514a23ecfe62903ad71c9e7e3cbe
Emilo <emilio.furnari87@gmail.com>
2020-11-30 14:48:38 +0100

    non funziona

---
+++ js/myscript.js
@@ -0,0 +1,32 @@
+// Esercizio: creare una to-do list, come quella fatta insieme stamattina, utilizzando Handlebars come templating engine.
+// Consiglio: fate uno step alla volta, prima caricate Handlebars da CDN in pagina, poi provate a seguire i primi step della sezione "Usage", modificando le proprietà dell'oggetto context.
+
+
+$(document).ready(
+  function () {
+
+    var list = [
+      "Fare la spesa",
+      "Fare i compiti",
+      "Fare il bucato",
+      "Dormire",
+      "Pagare le bollette"
+    ];
+
+    // COMPILO HANDLEBARS
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    // SCORRO NEL MIO ARRAY
+    for (var i = 0; i < list.length; i++) {
+      // DEFINISCO GLI ELEMENTI CHE VOGLIO GENERARE NEL HTML
+      var context = {
+        text: list[i]
+      };
+      //GENERO GLI ELEMENTI NEL HTML
+      var html = template(context);
+    }
+
+    //POSIZIONO I MIEI <LI> DENTRO <UL>
+    $("todo").append(html);
+});
