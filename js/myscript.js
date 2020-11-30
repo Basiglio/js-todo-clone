5ff8debe48afb27d516e73f3a73eab0eb745223c
Emilo <emilio.furnari87@gmail.com>
2020-11-30 16:10:34 +0100

    funziona

--- js/myscript.js
+++ js/myscript.js
@@ -5,38 +5,62 @@
 $(document).ready(
   function () {
 
+
+    // ESERCIZIO FATTO STAMATTINA
+
     // GENERO ARRAY LISTA
-    var list = [
-      "Fare la spesa",
-      "Fare i compiti",
-      "Fare il bucato",
-      "Dormire",
-      "Pagare le bollette",
-    ];
+    // var list = [
+    //   "Fare la spesa",
+    //   "Fare i compiti",
+    //   "Fare il bucato",
+    //   "Dormire",
+    //   "Pagare le bollette",
+    // ];
+
+    // // SCORRO NEL MIO ARRAY
+    // for (var i = 0; i < list.length; i++) {
+    //   // DEFINISCO GLI ELEMENTI CHE VOGLIO GENERARE NEL HTML
+    //   var context = {
+    //     text: list[i]
+    //   };
+    //   //GENERO GLI ELEMENTI NEL HTML
+    //   var html = template(context);
+    //   //AGGIUNGO I MIEI <LI> DENTRO <UL>
+    //   $("#todo").append(html);
+    // }
+
 
     // COMPILO HANDLEBARS
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
 
-    // SCORRO NEL MIO ARRAY
-    for (var i = 0; i < list.length; i++) {
-      // DEFINISCO GLI ELEMENTI CHE VOGLIO GENERARE NEL HTML
-      var context = {
-        text: list[i]
-      };
-      //GENERO GLI ELEMENTI NEL HTML
-      var html = template(context);
-      //AGGIUNGO I MIEI <LI> DENTRO <UL>
-      $("#todo").append(html);
-    }
+
+
+    // AGGANCIO ALLA PRESSIONE DELL'INVIO
+    $("input").keydown(
+      function (event){
+        console.log(event.keyCode);
+        if (event.keyCode == 13) {
+          var newText = $("input").val()
+          console.log(newText);
+          var context = {
+            text: newText
+          };
+          //GENERO GLI ELEMENTI NEL HTML
+          var html = template(context);
+          //AGGIUNGO IL MIO CONTENUTO ALL'HTML
+          $("#todo").append(html);
+        }
+      }
+    );
 
     // RENDO LA X CLICCABILE
-    $(".delete").click(
+    $(document).on("click", ".delete",
       function () {
         // RIMUOVO IL SUO GENITORE <LI> INSIEME ALLA X <SPAN>
         $(this).parent().remove();
       }
-    )
+    );
 
 
 
